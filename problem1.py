c32203027df6d5e381e748fa6f2d0d6402180193
gaifut <gaifut@gmail.com>
2023-01-29 17:17:36 +0300

    in this version the list of filtered multiples is wrong, I will make changes to re-write it

--- problem1.py
+++ problem1.py
@@ -8,12 +8,12 @@ for each_int in initial_range_to_work_with:
         #By throwing error
     def check_int(possible_multiple):
         try:
-            if int(possible_multiple):
+            if int(possible_multiple/first_value_for_multiples):
                 list_of_filtered_multiples.append(possible_multiple)
             else:
                 return 0
         except ValueError:
             return 0
-    possible_multiple = check_int(each_int/first_value_for_multiples)
+    check_int(each_int)
 
 print (list_of_filtered_multiples)
