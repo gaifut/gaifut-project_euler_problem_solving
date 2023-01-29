93380bbab3b548265b64ac307438324d53f47dbd
gaifut <gaifut@gmail.com>
2023-01-29 18:50:36 +0300

    Final solution

--- problem1.py
+++ problem1.py
@@ -1,19 +1,17 @@
-initial_range_to_work_with = list(range(1,10))
+initial_range_to_work_with = list(range(1,1000))
 list_of_filtered_multiples = list()
-print (initial_range_to_work_with )
 first_value_for_multiples = 3
 second_value_for_multiples = 5
 # checking for all the multiples of first multiple in the initial range
 for each_int in initial_range_to_work_with:
-        #By throwing error
-    def check_int(possible_multiple):
-        try:
-            if int(possible_multiple/first_value_for_multiples):
-                list_of_filtered_multiples.append(possible_multiple)
-            else:
+    def check_int(each_int):
+        muliple_check_first_value = each_int/first_value_for_multiples
+        multiple_check_second_value = each_int/second_value_for_multiples
+        whole_number_check = (muliple_check_first_value).is_integer() or (multiple_check_second_value).is_integer()
+        if whole_number_check == True:
+                list_of_filtered_multiples.append(each_int)
+        else:
                 return 0
-        except ValueError:
-            return 0
     check_int(each_int)
-
-print (list_of_filtered_multiples)
+sum_of_multiples = sum(list_of_filtered_multiples,0)
+print(sum_of_multiples)
