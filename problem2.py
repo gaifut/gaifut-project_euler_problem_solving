5b03a36e64f7328cc68227438868f4beff4fa3d1
gaifut <gaifut@gmail.com>
2023-04-30 22:13:46 +0300

    added problem 2

---
+++ problem2.py
@@ -0,0 +1,21 @@
+# Even Fibonacci numbers
+
+initial_range_to_work_with = list(range(1,4000000))
+fib_list = list()
+
+value1 = 0
+value2 = 1
+sum_fib_even = 0
+
+
+max_value = initial_range_to_work_with[len(initial_range_to_work_with)-1]
+
+while value1+value2 < max_value:
+    sum_fx = value1 + value2
+    if sum_fx%2 == 0:
+        sum_fib_even = sum_fib_even + sum_fx
+    value1 = value2
+    value2 = sum_fx
+
+print(sum_fib_even)
+
